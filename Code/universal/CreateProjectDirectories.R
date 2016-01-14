980195db13c4e58843b18ad1b3d764548f091b24
Filip Milojkovic <familojkovic@gmail.com>
2016-01-14 23:17:42 +0100

    updates

---
+++ Code/universal/CreateProjectDirectories.R
@@ -0,0 +1,16 @@
+#############################################
+# this file creates standard project directories
+#############################################
+
+getwd()
+
+dir.create("Code")
+dir.create("Code/universal")
+dir.create("Code/projectspecific")
+
+dir.create("Data")
+dir.create("Plots")
+dir.create("Reports")
+dir.create("Data/Raw")
+dir.create("Data/Processed")
+
